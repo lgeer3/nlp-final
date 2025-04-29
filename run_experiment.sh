888a774d4289fc2102dabaddb6a5d786720df71f
lgeer3 <lgeer3@jhu.edu>
2025-04-29 18:30:58 -0400

    fixed shell script to open right enviroment

--- run_experiment.sh
+++ run_experiment.sh
@@ -10,6 +10,6 @@
 #SBATCH --mem=16G
 
 module load anaconda
-conda activate hw7 # activate the Python environment
+conda activate nlp_final # activate the Python environment
 
 python run_experiment.py --dataset dogtooth/default_project_dev_test --batch_size 64 --vocab_trimming --vocab_size 10000 --hidden_dim 256 --hidden_layers 5 --block_size 64 --epochs 2 --learning_rate 1e-5 --save_model --save_path ./checkpoints/
