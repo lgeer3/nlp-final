fa885df72a3848fe06726c3d4abea154b5280b62
lgeer3 <lgeer3@jhu.edu>
2025-05-02 20:04:58 -0400

    batch_size = 64 block_size = 256 # context of up to 256 previous characters n_layer = 5 n_head = 6 n_embd = 300 dropout = 0.2 learning_rate = 1e-4

--- run_experiment.sh
+++ run_experiment.sh
@@ -14,7 +14,7 @@ module load anaconda
 
 
 echo "✅ Python about to run: $(date)"
-/home/cs601-lgeer3/scr4-cs601-dkhasha1/cs601-lgeer3/nlp-final/env/bin/python run_experiment.py --dataset dogtooth/default_project_dev_test --batch_size 64 --hidden_dim 384 --hidden_layers 6 --block_size 256 --epochs 3 --n_head 6 --learning_rate 1e-4 --save_model --save_path ./checkpoints/
+/home/cs601-lgeer3/scr4-cs601-dkhasha1/cs601-lgeer3/nlp-final/env/bin/python run_experiment.py --dataset dogtooth/default_project_dev_test --batch_size 64 --hidden_dim 300 --hidden_layers 5 --block_size 256 --epochs 3 --n_head 6 --learning_rate 1e-4 --save_model --save_path ./checkpoints/
 echo "✅ Python finished: $(date)";
 
 
